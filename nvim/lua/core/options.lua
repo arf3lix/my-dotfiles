555f07167dfd361f2e850031b2ccb299106ae440
arf3lix <felix.sanchez.dev@gmail.com>
2025-07-30 22:05:58 -0500

    bunch of stuff again

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,7 +1,11 @@
 vim.opt.expandtab = true
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.laststatus = 3
+
+vim.cmd('autocmd BufEnter * set formatoptions-=cro')
+vim.cmd('autocmd BufEnter * setlocal formatoptions-=cro')
---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,25 @@
+return {
+  {
+
+    "mason-org/mason.nvim",
+    opts = {}
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig' },
+}
+
+  }
+}
